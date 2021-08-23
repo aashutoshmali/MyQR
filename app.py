74623091865a348b9ddb69f812ce0d03a3742f61
unknown <aashutoshmali1460@gmail.com>
2021-08-23 11:18:45 +0530

    first commit

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template, request, redirect, url_for, session, s
 import pyqrcode
 import png
 from pyqrcode import QRCode
-from qrcode import get_qrcode
-
+import os
+import pyqrcode
 app = Flask(__name__)
 
 @app.route('/', methods =['GET', 'POST'])
@@ -14,10 +14,19 @@ def index():
 	else:
 		msg = 'Please Insert text !'
 		return render_template('index.html', msg = msg)
+def get_qrcode():
+    qr = pyqrcode.create('text')
+    qr.png('qr.png',scale = 8)
+    qr.svg("qr.svg", scale = 8)
+    return render_template('qr.html')
+
 @app.route('/download', methods=['GET','POST'])
 def downloadfile():
-      path = "/qr.png"
-      return send_file(path, as_attachment=True)
-
+     path = 'qr.png'
+     return send_file(path, as_attachment=True)
+@app.route('/save', methods=['GET','POST'])
+def save():
+     path = 'qr.svg'
+     return send_file(path, as_attachment=True)
 if __name__ == '__main__':
    app.run(debug=True)
--- qrcode.py
+++
@@ -1,6 +0,0 @@
-
-import pyqrcode
-def get_qrcode():
-    qr = pyqrcode.create('text')
-    qr.png('qr.png',scale = 8)
-    qr.svg("qr.svg", scale = 8) 
