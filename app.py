cbd4768e9011f4baa8325eb59d1602c8c3a7ef38
unknown <aashutoshmali1460@gmail.com>
2021-09-07 22:43:19 +0530

    mai

--- app.py
+++ app.py
@@ -3,22 +3,22 @@ import pyqrcode
 import png
 from pyqrcode import QRCode
 import os
-import pyqrcode
 app = Flask(__name__)
 
 @app.route('/', methods =['GET', 'POST'])
 def index():
 	if request.method == 'POST' and 'text' in request.form:
 		text = request.form['text']
-		return get_qrcode()
+		return get_qrcode(text=text)
 	else:
 		msg = 'Please Insert text !'
 		return render_template('index.html', msg = msg)
-def get_qrcode():
-    qr = pyqrcode.create('text')
+def get_qrcode(text):
+    qr = pyqrcode.create(text)
     qr.png('qr.png',scale = 8)
     qr.svg("qr.svg", scale = 8)
-    return render_template('qr.html')
+    img = 'qr.png'
+    return render_template('qr.html', text=text)
 
 @app.route('/download', methods=['GET','POST'])
 def downloadfile():
